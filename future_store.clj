24d3c885daf72dc2ddb2bb05f9f8967aed3e0a0d
Jeff Rose <rosejn@gmail.com>
2009-01-30 17:37:32 +0100

    Yes!  It works, and I understand laziness now.

--- future_store.clj
+++ future_store.clj
@@ -34,6 +34,7 @@
                                TraversalPosition
                                Traverser
                                Traverser$Order))
+  (:import (java.util.logging Logger Level))
   (:import (java.io File))
   (:use clojure.contrib.seq-utils))
   
@@ -50,10 +51,17 @@
 (def ALL ReturnableEvaluator/ALL)
 (def ALL-BUT-START ReturnableEvaluator/ALL_BUT_START_NODE)
 
+(def FS-LOG (Logger/getLogger "future-store-log"))
+(defn info    [& msg] (.info FS-LOG (apply str msg)))
+(defn warning [& msg] (.warning FS-LOG (apply str msg)))
+(defn severe  [& msg] (.severe FS-LOG (apply str msg)))
+
 (defn open-graph [path]
+  (info "(open-graph " path ")")
   (new EmbeddedNeo path))
 
 (defn close-graph [g]
+  (info "(close-graph)") 
   (.shutdown g))
 
 (declare delete-dir)
@@ -72,87 +80,120 @@
       (.delete dir))))
 
 (defn delete-graph [path]
+  (info "(delete-graph " path ")")
   (let [dir (new File path)]
     (delete-dir dir)))
 
+(def *g* nil)
 (def *tx* nil)
+
 (defn success [] (.success *tx*))
 (defn failure [] (.failure *tx*))
 
-(defmacro in-tx [g & body]
-  `(binding [*tx* (.beginTx ~g)]
+(defmacro with-graph [ #^String path & body ]
+  `(binding [*g* (open-graph ~path)]
+    (try ~@body
+      (finally (close-graph *g*)))))
+
+(defmacro in-tx [& body]
+  `(binding [*tx* (.beginTx *g*)]
      (try ~@body
        (catch Exception e# 
          (println "Exception in transaction: " e#)
          (throw e#))
        (finally (.finish *tx*)))))
 
-(defn get-root [g] 
-  (.getReferenceNode g))
+;; Do the enclosing operations inside a transaction if they are not already
+;; being performed in an outer transaction created by the user.
+(defmacro check-tx [& body]
+  `(if *tx* 
+     (do ~@body)
+     (do (in-tx *g* 
+                (let [result# ~@body]
+                  (success)
+                  result#)))))
 
-(defn get-node [g id]
-  (.getNodeById g id))
+(defn get-root [] 
+  (check-tx (.getReferenceNode *g*)))
 
-(defn get-edge [g id]
-  (.getRelationshipById g id))
+(defn get-node [id]
+  (check-tx (.getNodeById *g* id)))
+
+(defn get-edge [id]
+  (check-tx (.getRelationshipById *g* id)))
+
+(defn get-id [obj]
+  (.getId obj))
 
 (defn get-property [obj key]
-  (.getProperty obj (str key)))
+  (info "(get-property " (get-id obj) key ")")
+  (check-tx (.getProperty obj (str key))))
+
+(defn get-properties [obj]
+  (info "(get-properties " (get-id obj) ")")
+  (check-tx 
+    (seq (.getPropertyKeys obj))))
 
 (defn set-property [obj key value]
-  (.setProperty obj (str key) value))
+  (info "(set-property " (get-id obj) "{" key value "})")
+  (check-tx (.setProperty obj (str key) value)))
 
 ; These should return lazy sequences sitting on top of the java iterators
-(defn all-nodes [g])
-(defn all-edges [g])
+(defn all-nodes [])
+(defn all-edges [])
 
 ; These might have to actually count, or else we might need to keep track of
 ; the counts by hand if this is important, because I don't see a NEO method to
 ; get these values.
-(defn node-count [g])
-(defn edge-count [g])
+(defn node-count [])
+(defn edge-count [])
 
-(defn add-node [g & [props]]
-  (let [n (.createNode g)]
-    (map (fn [[k v]] (set-property n k v)) props)
-    n))
+(defn add-node [& [props]]
+  (info "(add-node " props ")")
+  (check-tx 
+    (let [n (.createNode *g*)]
+      (dorun (map (fn [[k v]] (set-property n k v)) props))
+    n)))
 
-(defn remove-node [g n]
-  (.delete g n))
+(defn remove-node [n]
+  (check-tx 
+    (.delete *g* n)))
 
 (defn- edge-type [#^Keyword n]
   (proxy [RelationshipType] []
     (name [] (name n))))
 
 (defn add-edge [#^Node src #^Keyword label #^Node dest & [props]]
-  (let [edge (.createRelationshipTo src dest (edge-type label))]
-    (map (fn [[k v]] (set-property edge k v)) props)))
+  (check-tx 
+    (let [edge (.createRelationshipTo src dest (edge-type label))]
+    (map (fn [[k v]] (set-property edge k v)) props))))
 
-(defn link-new [g src label & [props]]
-  (let [n (if props (add-node g props) (add-node g))]
+(defn link-new [src label & [props]]
+  (check-tx 
+    (let [n (if props (add-node props) (add-node))]
     (add-edge src label n)
-    n))
+    n)))
 
-(defn remove-edge [e]
-  (.delete e))
+(defn delete-edge [e]
+  (check-tx 
+    (.delete e)))
 
 (defn- edge-filter [label]
-  (println "filtering with label: " label)
+  (println "edge-filter " label ")")
   (fn [edge] (= label (keyword (.name (.getType edge))))))
 
 (defn- get-edges [n direction label]
-  (println "get-edges *tx*: " *tx*)
-  (let [edges (seq (.getRelationships n direction))]
-  (println "get-edges2 *tx*: " *tx*)
+  (info "(get-edges " (get-id n) direction label ")")
+  (check-tx (let [edges (seq (.getRelationships n direction))]
     (if label
       (filter (edge-filter label) edges)
-      edges)))
+      edges))))
 
 (defn in-edges [n & [label]]
   (get-edges n INCOMING label))
 
 (defn out-edges [n & [label]]
-  (println "out-edges label: " label)
+  (info "(out-edges " label ")")
   (get-edges n OUTGOING label))
 
 (defn in-nodes [n & [label]]
@@ -160,17 +201,18 @@
        (in-edges n label)))
 
 (defn out-nodes [n & [label]]
-  (println "out-nodes label: " label)
+  (info "(out-nodes " label ")")
   (map (fn [edge] (.getEndNode edge))
        (out-edges n label)))
 
 (defn path-query [start path]
-  (println "path: " path "\n*tx*: " *tx*)
+  (info "(path-query " (get-id start) path ")")
+  (check-tx
   (if (empty? path)
     start
     (let [label (first path)
           children (out-nodes start label)]
-      (map (fn [child] (path-query child (rest path))) children))))
+      (dorun (map (fn [child] (path-query child (rest path))) children))))))
 
 (defn dfs [start visitor]
   (map visitor 
@@ -185,29 +227,31 @@
 ;; Tests follow
 (use 'clojure.contrib.test-is)
 
-(defn add-n [g n]
+(defn add-n [n]
   (if (zero? n)
-    g
-    (recur (add-node g {:val n}) (dec n))))
+    nil
+    (do (add-node {:val n})
+      (recur (dec n)))))
 
 (deftest get-set-props []
-  (try 
-    (let [db (open-graph "test-db")]
-      (in-tx db (set-property (get-root db) "foo" 42))
-      (in-tx db (is (= 42 (get-property (get-root db) "foo")))))
-      (finally (delete-graph "test-db"))))
+  (with-graph "test-db"
+    (set-property (get-root) "foo" 42)
+    (is (= 42 (get-property (get-root) "foo")))
+    (info (str "Properties: " (get-properties (get-root))))
+    (let [n (add-node {"bar" 1234})]
+      (info (str "Properties: " (get-properties n)))
+      (is (= 1234 (get-property n "bar")))))
+  (delete-graph "test-db"))
 
 (deftest simple-query []
-  (try 
-    (let [db (open-graph "test-db")]
-      (in-tx db (do (link-new db (get-root db):foo {"value" 42}) (success)))
-;      (in-tx db (link-new db (link-new db (link-new db (link-new db root 
+  (with-graph "test-db"
+    (let [root (get-root)]
+      (link-new (get-root ) :foo {"value" 42})
+      (let [tgt (path-query root [:foo])]
+        (is (= 42 (.getProperty tgt "value"))))))
+  (delete-graph "test-db"))
+
+;      (in-tx (link-new (link-new (link-new (link-new root 
 ;                  :foo) :foo) :foo) :foo {:value 42})
 ;        (success))
-    (in-tx db (do (is (= 42 
-                     (.getProperty (first (path-query (get-root db) [:foo])) "value")))
-           (success))))
-         (finally (delete-graph "test-db"))))
-
-
 (defn fs-test [] (run-tests (find-ns 'future-store)))
